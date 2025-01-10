a2023df2b0d6ba03362cfab71049d0e0f36412b4
YJ-Kkang <kyujin995@gmail.com>
2025-01-11 04:14:40 +0900

    style: 리뷰 유효성 검사 추가
    - ReviewController에 @Valid 추가
    - Review entity의 rating Comment 괄호 추가

--- src/main/java/com/example/outsourcingproject/entity/Review.java
+++ src/main/java/com/example/outsourcingproject/entity/Review.java
@@ -11,6 +11,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import org.hibernate.annotations.Comment;
 
@@ -25,11 +26,12 @@ public class Review extends BaseEntity{
     @Column(columnDefinition = "BIGINT")
     private Long id;
 
-    @Comment("리뷰 별점(5점 만점")
+    @Comment("리뷰 별점(5점 만점)")
     @Column(
         name = "rating",
         nullable = false
     )
+    @NotNull
     @Min(value = 1, message = "별점은 1점 이상이어야 합니다.") // 최소값 1
     @Max(value = 5, message = "별점은 5점 이하여야 합니다.") // 최대값 5
     private Integer rating;
@@ -40,7 +42,6 @@ public class Review extends BaseEntity{
         nullable = false
     )
     private String contents;
-
 
     @Comment("손님 식별자")
     @ManyToOne(fetch = FetchType.LAZY)
--- src/main/java/com/example/outsourcingproject/review/controller/ReviewController.java
+++ src/main/java/com/example/outsourcingproject/review/controller/ReviewController.java
@@ -1,13 +1,15 @@
 package com.example.outsourcingproject.review.controller;
 
 import com.example.outsourcingproject.review.dto.request.CreateReviewRequestDto;
-import com.example.outsourcingproject.review.dto.response.CreateReviewResponseDto;
+import com.example.outsourcingproject.review.dto.response.ReviewResponseDto;
 import com.example.outsourcingproject.review.service.ReviewServiceImpl;
+import jakarta.validation.Valid;
+import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,18 +24,18 @@ public class ReviewController {
     }
 
     @PostMapping("/orders/{orderId}/reviews")
-    public ResponseEntity<CreateReviewResponseDto> createReviewAPI(
+    public ResponseEntity<ReviewResponseDto> createReviewAPI(
         @PathVariable("orderId") Long orderId,
-        @RequestBody CreateReviewRequestDto requestDto,
+        @Valid @RequestBody CreateReviewRequestDto requestDto,
         @RequestHeader("Authorization") String token //todo @Valid 유효성 검사
-        // @RequestAttribute String email 또는 @RequestAttribute Long userId하면 바로 값(이메일 또는 유저 아이디) 받아진다
-        // 이 받은 값을 매개변수로 서비스로 넘겨주면 바로 쓸 수 있다.
     ) {
-        CreateReviewResponseDto responseDto = reviewServiceImpl.createReviewService(
+        ReviewResponseDto responseDto = reviewServiceImpl.createReviewService(
             orderId,
             requestDto,
             token
         );
         return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
     }
+
+
 }
