6574b2faf927cc9ffd1107737363296f7f77ab60
backswan0 <rnrwprh3434@gmail.com>
2025-01-10 21:09:05 +0900

    feat: AOP 클래스를 생성하여 주문 상태 변경 로그 출력
    - [필요한 부분] 주문 요청 시각 출력 필요
    - [필요한 부분] 가게 식별자 출력 필요
    - [필요한 부분] 주문 생성 시 로그 기록할 AOP 필요

---
+++ src/main/java/com/example/outsourcingproject/aspect/OrderAspect.java
@@ -0,0 +1,37 @@
+package com.example.outsourcingproject.aspect;
+
+import com.example.outsourcingproject.order.dto.request.UpdateOrderRequestDto;
+import com.example.outsourcingproject.order.dto.response.UpdateOrderResponseDto;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class OrderAspect {
+
+    @Pointcut("execution(* com.example.outsourcingproject.order.service.OrderServiceImpl.updateOrderStatus(..))")
+    public void trackOrderServiceMethods() {
+    }
+
+    @Around("trackOrderServiceMethods()")
+    public Object trackOrderStatusUpdate(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        String methodName = joinPoint.getSignature().getName();
+        Object[] args = joinPoint.getArgs();
+
+        System.out.println(methodName + " method starts");
+
+        Object result = joinPoint.proceed();
+
+        UpdateOrderResponseDto responseDto = (UpdateOrderResponseDto) result;
+        UpdateOrderRequestDto requestDto = (UpdateOrderRequestDto) args[0];
+
+        System.out.println("Order ID: " + requestDto.getId());
+        System.out.println("Updated Order Status: " + responseDto.getUpdatedOrderStatus());
+
+        return result; // todo 주문 요청 시각이랑 가게 id 출력도 같이 해야 함
+    }
+}
