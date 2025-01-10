25b60877e568a62f631dfc868ea96670e631fa14
YJ-Kkang <kyujin995@gmail.com>
2025-01-11 05:44:56 +0900

    feat: 리뷰 다건조회 기능 추가

--- src/main/java/com/example/outsourcingproject/review/controller/ReviewController.java
+++ src/main/java/com/example/outsourcingproject/review/controller/ReviewController.java
@@ -1,7 +1,7 @@
 package com.example.outsourcingproject.review.controller;
 
 import com.example.outsourcingproject.review.dto.request.CreateReviewRequestDto;
-import com.example.outsourcingproject.review.dto.response.ReviewResponseDto;
+import com.example.outsourcingproject.review.dto.response.CreateReviewResponseDto;
 import com.example.outsourcingproject.review.service.ReviewServiceImpl;
 import jakarta.validation.Valid;
 import java.util.List;
@@ -24,12 +24,12 @@ public class ReviewController {
     }
 
     @PostMapping("/orders/{orderId}/reviews")
-    public ResponseEntity<ReviewResponseDto> createReviewAPI(
+    public ResponseEntity<CreateReviewResponseDto> createReviewAPI(
         @PathVariable("orderId") Long orderId,
         @Valid @RequestBody CreateReviewRequestDto requestDto,
         @RequestHeader("Authorization") String token //todo @Valid 유효성 검사
     ) {
-        ReviewResponseDto responseDto = reviewServiceImpl.createReviewService(
+        CreateReviewResponseDto responseDto = reviewServiceImpl.createReviewService(
             orderId,
             requestDto,
             token
@@ -37,5 +37,12 @@ public class ReviewController {
         return new ResponseEntity<>(responseDto, HttpStatus.CREATED);
     }
 
+    // 리뷰 다건 조회(가게 정보 기준, 최신순 정렬)
+    // 리뷰 별점 범위에 따라 조회 가능
+    @GetMapping
+    public List<CreateReviewResponseDto> findAllReview() {
+    List<CreateReviewResponseDto> findAllCreateReviewResponseDtoList = reviewServiceImpl.findAllReviewService();
+    return findAllCreateReviewResponseDtoList;
+    }
 
 }
--- src/main/java/com/example/outsourcingproject/review/service/ReviewServiceImpl.java
+++ src/main/java/com/example/outsourcingproject/review/service/ReviewServiceImpl.java
@@ -13,6 +13,8 @@ import com.example.outsourcingproject.review.repository.ReviewRepository;
 import com.example.outsourcingproject.store.repository.StoreRepository;
 import com.example.outsourcingproject.utils.JwtUtil;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -36,34 +38,39 @@ public class ReviewServiceImpl{
     ) {
         // 리뷰 데이터를 저장하기 위한 사전 작업
         // 주문 조회
-        Order foundorder = orderRepository.findById(orderId)
+        Order findorder = orderRepository
+            .findById(orderId)
             .orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.NOT_FOUND)
             ); // todo 오더 식별자로 주문 조회했을 때 주문이 없으면 예외 처리
 
         // 주문 상태가 배송 완료일 경우에만 리뷰를 쓸 수 있도록 예외처리
-        boolean isNotDeliverd = !foundorder.getOrderStatus().equals(OrderStatus.DELIVERED);
+        boolean isNotDeliverd = !findorder
+            .getOrderStatus()
+            .equals(OrderStatus.DELIVERED
+            );
 
         if (isNotDeliverd) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND); // todo OrderStatus가 배송완료 아닌 경우 예외 처리
         }
 
         // 가게 정보 가져오기
-        Store foundStore = foundorder.getStore();
+        Store findStore = findorder.getStore();
 
         // jwt 토큰에 저장된 손님 이메일 추출
         String customerEmail = jwtUtil.extractCustomerEmail(token);
 
         // 손님 이메일로 손님 아이디 추출
-        Customer foundCustomer = customerAuthRepository.findByEmail(customerEmail)
+        Customer findCustomer = customerAuthRepository
+            .findByEmail(customerEmail)
             .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND)
             ); // todo 손님 이메일로 찾을 수 있는 아이디 없으면 예외 처리
 
         // Dto에서 리뷰 내용 가져오고, 리뷰 엔티티 만들어주기(엔티티 == 테이블)
         Review reviewToSave = new Review(
-            foundCustomer,
-            foundStore,
-            foundorder,
+            findCustomer,
+            findStore,
+            findorder,
             requestDto.getContents(),
             requestDto.getRating()
         );
@@ -75,7 +82,7 @@ public class ReviewServiceImpl{
         Long savedReviewId = savedReview.getId();
         LocalDateTime savedLocalDateTime = savedReview.getCreatedAt();
 
-        return new CreateReviewResponseDto (
+        return new CreateReviewResponseDto(
             savedReviewId,
             requestDto.getContents(),
             requestDto.getRating(),
@@ -83,5 +90,18 @@ public class ReviewServiceImpl{
         );
     }
 
+    public List<CreateReviewResponseDto> findAllReviewService() {
+        // DB에서 가져오기
+        List<Review> reviewList = new ArrayList<>();
+        reviewList = reviewRepository
+            .findAll();
 
+        // dtoList로 변환하기 위해 선언
+        List<CreateReviewResponseDto> createReviewResponseDtoList = new ArrayList<>();
+
+        for (Review review : reviewList) {
+            CreateReviewResponseDto createReviewResponseDto = new CreateReviewResponseDto(reviewList);
+
+        }
+    }
 }
