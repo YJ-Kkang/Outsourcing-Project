78e2b9f9b82d70a752d71b5b306541c8f66b1457
backswan0 <rnrwprh3434@gmail.com>
2025-01-10 13:56:01 +0900

    feat: 각 주문의 상세 목록 조회 기능 추가 (CRUD의 R, 사장님 권한 추가 필요)

--- src/main/java/com/example/outsourcingproject/orderitem/controller/OrderItemController.java
+++ src/main/java/com/example/outsourcingproject/orderitem/controller/OrderItemController.java
@@ -1,11 +1,14 @@
 package com.example.outsourcingproject.orderitem.controller;
 
-import com.example.outsourcingproject.common.OrderItemWrapper;
+import com.example.outsourcingproject.common.CreateOrderItemWrapper;
+import com.example.outsourcingproject.common.ReadOrderItemWrapper;
 import com.example.outsourcingproject.orderitem.dto.request.CreateOrderItemRequestDto;
 import com.example.outsourcingproject.orderitem.service.OrderItemServiceImpl;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,25 +16,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/stores/{storeId}/orders")
+@RequiredArgsConstructor
 public class OrderItemController {
 
     private final OrderItemServiceImpl orderItemService;
 
-    public OrderItemController(OrderItemServiceImpl orderItemService) {
-        this.orderItemService = orderItemService;
-    }
-
-    @PostMapping("/stores/{storeId}/orders")
-    public ResponseEntity<OrderItemWrapper> createOrderItem(
-        @PathVariable Long storeId,
+    @PostMapping()
+    public ResponseEntity<CreateOrderItemWrapper> createOrderItem(
+        @PathVariable("storeId") Long storeId,
         @RequestBody List<CreateOrderItemRequestDto> requestDtoList
     ) {
-        OrderItemWrapper responseDtoWrapper = orderItemService.createOrderItem(
+        CreateOrderItemWrapper responseDtoWrapper = orderItemService.createOrderItem(
             storeId,
             requestDtoList
         );
 
         return new ResponseEntity<>(responseDtoWrapper, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{orderId}")
+    public ResponseEntity<ReadOrderItemWrapper> readAllOrderItems(
+        @PathVariable("orderId") Long orderId
+    ) {
+        ReadOrderItemWrapper responseDtoWrapper = orderItemService.readAllOrderItemsByOrderId(orderId);
+
+        return new ResponseEntity<>(responseDtoWrapper, HttpStatus.OK);
     }
 }
