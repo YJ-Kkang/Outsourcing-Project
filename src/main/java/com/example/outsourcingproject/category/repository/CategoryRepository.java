3d3b8a37eee5c7b187fe28fa1ff832b69d0921ed
backswan0 <rnrwprh3434@gmail.com>
2025-01-12 01:25:24 +0900

    feat: 메뉴 카테고리 생성 기능 구현
    - 도전 단계 중 메뉴 강화 기능을 구현하고자 카테고리 구현

---
+++ src/main/java/com/example/outsourcingproject/category/repository/CategoryRepository.java
@@ -0,0 +1,8 @@
+package com.example.outsourcingproject.category.repository;
+
+import com.example.outsourcingproject.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
