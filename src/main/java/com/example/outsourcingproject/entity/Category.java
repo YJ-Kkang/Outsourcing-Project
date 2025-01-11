ca76ac1f060520bf6bffcf45753c39e99ade0865
backswan0 <rnrwprh3434@gmail.com>
2025-01-12 04:41:06 +0900

    style: 코드 클린업
    - 팀 컨벤션에 맞추어 코드 줄 맞춤 및 어노테이션 사용
    
    - 불필요한 메서드 삭제

--- src/main/java/com/example/outsourcingproject/entity/Category.java
+++ src/main/java/com/example/outsourcingproject/entity/Category.java
@@ -2,15 +2,18 @@ package com.example.outsourcingproject.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import org.hibernate.annotations.Comment;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @Table(name = "categories")
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 public class Category {
 
